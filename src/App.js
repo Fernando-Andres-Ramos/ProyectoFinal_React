a10f2bd7e7b99f42a177644b8d2d6117ca6bf9ec
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-12-05 23:00:26 -0300

    carpeta components creada con archivo NavBar.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const style = {
+    color: 'green',
+    border: 10
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +18,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style = {style}
         >
           Learn React
         </a>
