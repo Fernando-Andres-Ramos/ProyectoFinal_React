29d4339cc9ba8b6c879fe79ee27a01c3366b1657
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-12-06 23:18:59 -0300

    componente NavBar creado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/NavBar';
 import NavBar from './components/NavBar';
 
 function App() {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,24 @@
+import './NavBar.css';
 function NavBar() {
 
   return (
-    <div>
-      <h1>1d10 Games</h1>
-      <nav>
-        <h2>Categorias</h2>
-        <ul>
-          <li><a href="./">Juegos de mesa</a></li>
-          <li><a href="./">Cartas</a></li>
-          <li><a href="./">Miniaturas</a></li>
-          <li><a href="./">Preventas y Ofertas</a></li>
-          <li><a href="./">Contacto</a></li>
+    <div className="Conteiner-NavBar">
+      <h1 className='Titulo'>1d10 Games</h1>
+      <nav className='Nav'>
+        <div style={{display:'flex',justifyContent:'space-between'}}>
+          <a style={{fontSize:30,textDecoration:'none'}} href="./">Inicio</a>
+          <ul className="OpcionesDeUsuario">
+            <li><a className="Nav-List_link" href="./">Log in</a></li>
+            <li><a className="Nav-List_link" href="./">Sign up</a></li>
+            <li><a className="Nav-List_link" href="./">Carrito</a></li>
+          </ul>
+        </div>
+        <ul className="Nav-List">
+          <li><a className="Nav-List_link" href="./">Juegos de mesa</a></li>
+          <li><a className="Nav-List_link" href="./">Cartas</a></li>
+          <li><a className="Nav-List_link" href="./">Miniaturas</a></li>
+          <li><a className="Nav-List_link" href="./">Preventas y Ofertas</a></li>
+          <li><a className="Nav-List_link" href="./">Contacto</a></li>
         </ul>
       </nav>
     </div>
