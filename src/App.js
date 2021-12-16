0b8eb2c0b5fd0a8867dba65fa0eb56772af4a1c1
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-12-15 21:04:34 -0300

    creado un array de datos de productos(mock)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar/NavBar';
 import { ItemListContainer } from './containers/ItemListContainer/ItemListCointainer';
 import { ItemCount } from './components/ItemCount/ItemCount';
+
+
+
+const PRODUCTOS = [{
+  id: 1,
+  title: "Ajedrez",
+  description:"Clasico juego de ajedrez para disfrutar",
+  price: 500
+},{
+  id:2,
+  title:"Cartas",
+  description: "Juego de cartas para toda la familia",
+  price: 300
+},{
+  id:3,
+  title: "Shenga",
+  description:"Pone a prueba tu concentración, o simplemente reite con tus amigos",
+  price: 400,
+},{
+  id:4,
+  title: "Monopoly",
+  description: "El famoso juego Monopoly",
+  price: 9000,
+}]
+
+
+
 function App() {
 
   const Mensaje ='¡Bienvenid@ a nuestra tienda! ¡Aqui podras comprar juegos de mesa, cartas y más! (Sitio en construcción)';
