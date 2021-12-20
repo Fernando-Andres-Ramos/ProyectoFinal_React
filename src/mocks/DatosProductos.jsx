5caa1cdc22c902750ed305d072d125c14af98621
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-12-20 20:15:58 -0300

    agregadas URLs de imagenes a los productos

--- src/mocks/DatosProductos.jsx
+++ src/mocks/DatosProductos.jsx
@@ -2,28 +2,28 @@ export const DatosProductos = [{
   id: 1,
   title: "Ajedrez",
   description:"Juego de mesa de estrategia, 32 piezas",
-  img: 'https://pbs.twimg.com/media/DR37eAoW4AAqRPI.jpg',
+  img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg/1200px-Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg',
   price: 500,
   stock: 15
 },{
   id:2,
   title:"Cartas",
   description: "Juego de cartas con 50 unidades",
-  img: 'https://i.pinimg.com/originals/3e/90/8e/3e908ede0b7cddacc35ff0d0aeb55fe3.gif',
+  img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg/1200px-Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg',
   price: 300,
   stock: 20
 },{
   id:3,
   title: "Shenga",
   description:"Juego de bloques con 50 piezas",
-  img: 'https://ih1.redbubble.net/image.38456508.9561/raf,750x1000,075,t,469C4A:e72f2495dd.jpg',
+  img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg/1200px-Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg',
   price: 400,
   stock: 10
 },{
   id:4,
   title: "Monopoly",
   description: "El famoso juego Monopoly",
-  img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo-ICeSIG1qbWiImzJ2ptQRx3WuCRyNod7O7yOq1lXRXxp3KRTsEfBla0ccIsiprky3Tk&usqp=CAU',
+  img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg/1200px-Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg',
   price: 9000,
   stock: 5
 }]
