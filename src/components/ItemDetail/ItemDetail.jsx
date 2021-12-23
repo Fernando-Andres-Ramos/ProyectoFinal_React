e7067238a5b85dba363a822438197693b7a4b415
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-12-23 01:42:28 -0300

    estilos para ItemDetails y links para navegar entre routes agregados

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import styles from "./ItemDetail.module.css"
 import { ItemCount } from "../ItemCount/ItemCount";
+import { Link } from "react-router-dom";
 
 
 export const ItemDetail = (item) => {
   return (
     <div className={styles.product}>
-      <div>
+      <div className={styles.imgAndCount}>
         <img className={styles.imgProductos} src={item.dato.pictureURL} alt={`Imagen de ${item.dato.title}`}/>
+        <ItemCount stock={item.dato.stock} onAdd={count => console.log(`Agregaste al carrito ${count}u. de ${item.dato.title}. Por un valor de ${count*item.dato.price}$`)}/>
       </div>
       <div className={styles.infoProducto}>
-        <h3 className={styles.subtituloProduct}>{item.dato.title}</h3>
-        <p>{item.dato.description}</p>
-        <h4 className={styles.precio}>${item.dato.price}</h4>
-        <h6 className={styles.stock}>Stock:{item.dato.stock}</h6>
-        <div>
-          <ItemCount stock={item.dato.stock} onAdd={count => console.log(`Agregaste al carrito ${count}u. de ${item.dato.title}. Por un valor de ${count*item.dato.price}$`)}/>
-        </div>
+        <h3>Detalles del producto</h3>
+        <p className={styles.subtituloProduct}><b>Nombre: </b>{item.dato.title}</p>
+        <p><b>Descripción : </b>{item.dato.description}</p>
+        <p className={styles.precio}><b>Precio del producto: </b>${item.dato.price}</p>
       </div>
-      <button style={{cursor:"pointer"}} className={styles.botonDetalles}>Regresar</button>      
+      <Link to={`/`} className={styles.botonRegresar}>Volver</Link>      
     </div>
   )
 }
