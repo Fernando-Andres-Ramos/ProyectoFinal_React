861b683799ca81117fe0066ddb213dfb07552a5e
Fernando Ramos <fernando-ramos94@hotmail.com>
2021-12-23 02:01:04 -0300

    se agrega la propiedad category al mock de productos

--- src/mocks/DatosProductos.jsx
+++ src/mocks/DatosProductos.jsx
@@ -4,26 +4,30 @@ export const DatosProductos = [{
   description:"Juego de mesa de estrategia, 32 piezas",
   pictureURL:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg/1200px-Chess_board_with_chess_set_in_opening_position_2012_PD_03.jpg",
   price: 500,
-  stock: 15
+  stock: 15,
+  category:"JuegoDeMesa"
 },{
   id:2,
   title:"Cartas",
   description: "Juego de cartas con 50 unidades",
   pictureURL:"https://http2.mlstatic.com/D_NQ_NP_730111-MLA46598736486_072021-O.webp",
   price: 300,
-  stock: 20
+  stock: 20,
+  category:"JuegoDeCartas"
 },{
   id:3,
   title: "Jenga",
   description:"Juego de bloques con 50 piezas",
   pictureURL:"https://m.media-amazon.com/images/I/71Df1l5bb1L._AC_SL1024_.jpg",
   price: 400,
-  stock: 10
+  stock: 10,
+  category:"JuegoDeMesa"
 },{
   id:4,
   title: "Monopoly",
   description: "El famoso juego Monopoly",
   pictureURL:"https://jugueteriaelpehuen.com.ar/images/productos/1570897103630509528134.jpg",
   price: 9000,
-  stock: 5
+  stock: 5,
+  category:"JuegoDeMesa"
 }]
