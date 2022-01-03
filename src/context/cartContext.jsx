e8b6eb6081aab5d17bb302ae8e4d86f3c8fe8f91
Fernando <fernando-ramos94@hotmail.com>
2022-01-03 02:13:20 -0300

    metodos para CartContext definidos

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -1,3 +1,31 @@
 import React from "react";
 
-export const ThemeContext = React.createContext([])
+export const CartContext = React.createContext()
+
+export const CustomProvider = ({defaultValue=[],children}) => {
+
+
+    function addItem(item,quantity){
+
+    }
+
+    function removeItem(itemId){
+
+    }
+
+    function clear(){
+
+    }
+
+    function isInCart(id){
+        (id)?true:false;
+    }
+
+    return(
+        <CartContext.Provider value={addItem,removeItem,clear,isInCart}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+
