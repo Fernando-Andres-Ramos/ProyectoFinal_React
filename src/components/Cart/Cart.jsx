7fb5d9dcdc3cf4d87529dd4c72afe7a15787202a
Fernando Ramos <fernando-ramos94@hotmail.com>
2022-01-05 18:14:20 -0300

    Estilos para Cart y CartContainer añadidos

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { useContext } from "react";
-import { CartContext } from "../../context/cartContext";
+import styles from "./Cart.module.css"
 
-
-export function Cart (){
-    let [,,,,compra] = useContext (CartContext)
-    console.log(compra)
+export function Cart (producto){
+  console.log(producto.item.dato.title)
     return(
       <>
-        {compra.length!=0?console.log("Hay cosas en el carrito"):console.log("El carrito esta vacio")}
+        <div className={styles.container}>
+          <div className={styles.miniDetalle}>
+            <p>{producto.item.dato.title}</p>
+            <img className={styles.miniImagen} src={producto.item.dato.pictureURL} alt="imagen_producto" />
+          </div>
+          <div className={styles.numeros}>
+            <p className={styles.detail}>{producto.quantity}</p>
+            <p className={styles.detail}>{(producto.quantity)*(producto.item.dato.price)}$</p>
+          </div>
+        </div>
       </>
     )
 }
--- src/containers/CartContainer/CartContainer.jsx
+++ src/containers/CartContainer/CartContainer.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import styles from "./CartContainer.module.css"
 import { Cart } from "../../components/Cart/Cart";
+import { useContext } from "react";
+import { CartContext } from "../../context/cartContext";
+
+
+
+
 
 export function CartContainer(){
+  
+  let [,,,,compra] = useContext (CartContext)
+  console.log(compra)
+
   return(
     <>
-      <h1 className={styles.tituloCart}>USTED HA INGRESADO AL CARRITO DE COMPRAS (EN CONSTRUCCIÓN)</h1>
-      <Cart></Cart>
+      <div className={styles.container}>
+        <h1 className={styles.tituloCart}>Sus compras actuales</h1>
+        <div className={styles.titulosColumnas}>
+            <p >Lista de los productos</p>
+            <div className={styles.detallesCompra}>
+              <p className={styles.detail}>Cantidad solicitada</p>
+              <p className={styles.detail}>Precios a pagar</p>
+            </div>
+          <div className={styles.listaProductos} >
+            {compra.length!=0
+            ?compra.map(producto=><Cart {...producto}/>)
+            :<div><p>El carrito esta vacio</p></div>}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
