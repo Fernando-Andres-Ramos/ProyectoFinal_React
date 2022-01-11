41e6e93e6a23c5a282038ca063228dfc58a88ff0
Fernando Ramos <fernando-ramos94@hotmail.com>
2022-01-11 15:43:09 -0300

    globo contador agregado al cartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useContext } from "react/cjs/react.development";
 import {ImageCarrito} from "../../assets/images/images";
+import { CartContext } from "../../context/cartContext";
 import { Carrito } from "../ImgCarrito/ImgCarrito";
 import styles from './CartWidget.module.css'
 
 export const CardWidget = () =>{
+
+  const [,,,,,,cantidad] = useContext(CartContext)
+
   return (
     <div className={styles.flexConteiner}>
       <Carrito {...ImageCarrito.img1}/>
+      {cantidad!=0
+      ?<div className={styles.globoCompra}>{cantidad}</div>
+      :<div></div>}
+
     </div>
   );
 }
