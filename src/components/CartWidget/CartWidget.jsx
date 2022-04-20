582015bfe94ed6e760e75521204466d6225e1af1
Fernando Ramos <fernando-ramos94@hotmail.com>
2022-04-20 02:07:40 -0300

    error en import de useContext en CartWidget revisado

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { useContext } from "react/cjs/react.development";
+import { useContext } from "react";
 import {ImageCarrito} from "../../assets/images/images";
 import { CartContext } from "../../context/cartContext";
 import { Carrito } from "../ImgCarrito/ImgCarrito";
 import styles from './CartWidget.module.css'
 
 export function CartWidget (){
-  // const [addItem,removeItem,clear,isInCart,items,totalPrice,totalCount,userState,logUser] = useContext(CartContext)
+  const [addItem,removeItem,clear,isInCart,items,totalPrice,totalCount,userState,logUser] = useContext(CartContext)
   return (
     <div className={styles.flexConteiner}>
+      {/* <Carrito {...ImageCarrito.img1}/> */}
       <img src="https://cdn-icons-png.flaticon.com/512/107/107831.png" alt="una imagen" width={"50px"} />
-      {/* <Carrito {...ImageCarrito.img1}/>
       {totalCount!==0
       ?<div className={styles.globoCompra}>{totalCount}</div>
-      :<div></div>} */}
+      :<div></div>}
     </div>
   );
 }
